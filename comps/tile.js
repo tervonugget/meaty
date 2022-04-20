115ba85f7185cbc32100cf2c798837acf8b7aea6
Trevor Lee <tlee297@my.bcit.ca>
2022-04-19 18:44:01 -0700

    fetch data for tiles depending on the current path

--- comps/tile.js
+++ comps/tile.js
@@ -1,5 +1,8 @@
+import Image from "next/image";
 import {useRouter} from "next/router";
 import styled from "styled-components";
+
+import {animals} from '../data/selections';
 
 
 // Styled components
@@ -7,12 +10,35 @@ let Container = styled.div`
   width: 80%;
 
   margin-left: 10%;
+  margin-bottom: 1rem;
 
   border: 2px blue solid;
+  text-align: center;
 `;
 
+
 export default function Tile(){
+  // Defining variables
+  let router = useRouter();
+  
+  // Check the router's current path and display the corresponsding
+  // information from an array in /data/selections.js
+  if (router.asPath === '/'){
+    return <div>
+      <Container>
+        <h3>Chicken</h3>
+        <p>{animals[0].title}</p>
+      </Container>
+      <Container>
+        <h3>Cow</h3>
+      </Container>
+      <Container>
+        <h3>Pig</h3>
+      </Container>
+    </div>
+  }
+  
   return <Container>
-    <h1>Uh oh!</h1>
+    <h3>Uh oh!</h3>
   </Container>
 }
---
+++ data/selections.js
@@ -0,0 +1,15 @@
+// Animals
+export const animals = [
+  {
+    route: 'ch',
+    title: 'Chicken'
+  },
+  {
+    route: 'co',
+    title: 'Cow'
+  },
+  {
+    route: 'pi',
+    title: 'Pig'
+  }
+]
